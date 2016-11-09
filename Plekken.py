2d2ead1c8a785a2adb450a49430b772f1292c0ab
Meifeng <tickets_to_life@hotmail.com>
2016-11-09 21:08:39 +0100

    Plekken GUI Update

--- Plekken.py
+++ Plekken.py
@@ -1,4 +1,12 @@
 import csv
+from tkinter import *
+import sys
+
+root = Tk()
+
+def close():
+    sys.exit()
+
 nietbeschikbaar = 0
 file = open('fietsen.csv', 'r', newline='')
 tekst = csv.reader(file)
@@ -7,6 +15,29 @@ for regel in tekst:
 beschikbaar = 12 - nietbeschikbaar
 
 if beschikbaar > 0:
-    print('er zijn '+ str(beschikbaar)+ 'plekken beschikbaar')
+    message1 = Label(master=root,
+                    text='Beschikbare plekken \n\n\nEr zijn nog {} plekken beschikbaar'.format(beschikbaar),
+                    background='yellow',
+                    foreground='blue',
+                    font=('Helvetica', 20, 'bold'),
+                    anchor=N,
+                    width=35,
+                    height=11,)
+    message1.pack()
+    button1 = Button(master=root, text='Menu', font=('Helvetica', 20, 'bold'), borderwidth=0, background='yellow', foreground='blue', command=close)
+    button1.place(x=40, y=300)
 else:
-    print('er zijn geen plekken meer vrij')
+    message2 = Label(master=root,
+                    text='Beschikbare plekken \n\n\nEr zijn momenteel geen plekken meer vrij',
+                    background='yellow',
+                    foreground='blue',
+                    font=('Helvetica', 20, 'bold'),
+                    anchor=N,
+                    width=35,
+                    height=11,)
+    message2.pack()
+    button2 = Button(master=root, text='Menu', font=('Helvetica', 20, 'bold'), borderwidth=0, background='yellow', foreground='blue', command=close)
+    button2.place(x=40, y=300)
+
+
+root.mainloop()
