e51ac5250c517f67919607fb904236a8a3ee248f
Meifeng <tickets_to_life@hotmail.com>
2016-11-09 21:02:18 +0100

    Stallen GUI Update

--- Stallen.py
+++ Stallen.py
@@ -1,1 +1,20 @@
-print('deze optie werkt nog niet')
+from tkinter import *
+import sys
+root = Tk()
+
+def close():
+    sys.exit()
+
+message = Label(master=root,
+                    text='Stallen \n\n\nDeze optie werkt nog niet',
+                    background='yellow',
+                    foreground='blue',
+                    font=('Helvetica', 20, 'bold'),
+                    anchor=N,
+                    width=35,
+                    height=11,)
+message.pack()
+button1 = Button(master=root, text='Menu', font=('Helvetica', 20, 'bold'), borderwidth=0, background='yellow', foreground='blue', command=close)
+button1.place(x=40, y=300)
+
+root.mainloop()
