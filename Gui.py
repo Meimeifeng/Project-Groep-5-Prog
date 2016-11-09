940a05f3669db5f93f42c47e2ac68696aa2da6ad
Meifeng <tickets_to_life@hotmail.com>
2016-11-09 16:45:34 +0100

    Menu GUI Update

--- Gui.py
+++ Gui.py
@@ -12,7 +12,7 @@ label = Label(master=root,
                 height=8,)
 label.pack()
 
-button1 = Button(master=root, text='Nieuwe fiets registeren', background='yellow', foreground='blue')
+button1 = Button(master=root, text='Nieuwe fiets registeren', background='yellow', foreground='blue', )
 button1.place(x=130, y=50)
 button2 = Button(master=root, text='Stallen', background='yellow', foreground='blue')
 button2.place(x=165, y=85)
